1789ec87ba8bda97bd85eb2466cdccf635785ae6
Pwl Webdev <pwl.webdev@gmail.com>
2016-09-17 20:33:36 +0200

    Calculaor buttons and onclick actions added

---
+++ assets/scripts/script.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+
+	$( "#1" ).click(function() {
+	  console.log("1");
+	});
+	$( "#2" ).click(function() {
+	  console.log("2");
+	});
+	$( "#3" ).click(function() {
+	  console.log("3");
+	});
+	$( "#4" ).click(function() {
+	  console.log("4");
+	});
+	$( "#5" ).click(function() {
+	  console.log("5");
+	});
+	$( "#6" ).click(function() {
+	  console.log("6");
+	});
+	$( "#7" ).click(function() {
+	  console.log("7");
+	});
+	$( "#8" ).click(function() {
+	  console.log("8");
+	});
+	$( "#9" ).click(function() {
+	  console.log("9");
+	});
+	$( "#0" ).click(function() {
+	  console.log("0");
+	});
+	$( "#plus" ).click(function() {
+	  console.log("plus");
+	});
+	$( "#minus" ).click(function() {
+	  console.log("minus");
+	});
+	$( "#multiply" ).click(function() {
+	  console.log("multiply");
+	});
+	$( "#divide" ).click(function() {
+	  console.log("divide");
+	});
+	$( "#period" ).click(function() {
+	  console.log("period");
+	});
+	$( "#c" ).click(function() {
+	  console.log("c");
+	});
+	$( "#equals" ).click(function() {
+	  console.log("equals");
+	});
+});
+
